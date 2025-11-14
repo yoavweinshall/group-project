e059433b7be4b2d339383a1e3f78837aa3d01352
yoavweinshall <yoavweinshall@gmail.com>
2025-11-14 10:36:54 -0800

    documentation for functions

--- chesslab/ai/ai.py
+++ chesslab/ai/ai.py
@@ -1,7 +1,7 @@
 """
 Core AI routines for ChessLab.
 
-All of the student work for this assignment lives in this file. Implement the
+All the student work for this assignment lives in this file. Implement the
 functions below so that:
   * `evaluate` returns a heuristic score for the given board state.
   * `choose_minimax_move` picks a move via minimax search (no pruning).
@@ -19,6 +19,68 @@ from ..board import Board, Move
 from ..common.profiling import Counter
 
 MoveType = Tuple[Tuple[int, int], Tuple[int, int], Optional[str]]
+PIECES_SCORE = {'P':100, 'N': 320, 'B': 330, 'R': 500, 'Q': 900, 'K': 20000}
+PIECES_LOCATION_SCORE = {'P':[
+                        [0,  0,  0,  0,  0,  0,  0,  0],
+                        [50, 50, 50, 50, 50, 50, 50, 50],
+                        [10, 10, 20, 30, 30, 20, 10, 10],
+                        [5,  5, 10, 25, 25, 10,  5,  5],
+                        [0,  0,  0, 20, 20,  0,  0,  0],
+                        [5, -5,-10,  0,  0,-10, -5,  5],
+                        [5, 10, 10,-20,-20, 10, 10,  5],
+                        [0,  0,  0,  0,  0,  0,  0,  0]
+                    ],
+                'N': [
+                        [-50,-40,-30,-30,-30,-30,-40,-50],
+                        [-40,-20,  0,  0,  0,  0,-20,-40],
+                        [-30,  0, 10, 15, 15, 10,  0,-30],
+                        [-30,  5, 15, 20, 20, 15,  5,-30],
+                        [-30,  0, 15, 20, 20, 15,  0,-30],
+                        [-30,  5, 10, 15, 15, 10,  5,-30],
+                        [-40,-20,  0,  5,  5,  0,-20,-40],
+                        [-50,-40,-30,-30,-30,-30,-40,-50]
+                    ],
+                'B': [
+                        [-20,-10,-10,-10,-10,-10,-10,-20],
+                        [-10,  0,  0,  0,  0,  0,  0,-10],
+                        [-10,  0,  5, 10, 10,  5,  0,-10],
+                        [-10,  5,  5, 10, 10,  5,  5,-10],
+                        [-10,  0, 10, 10, 10, 10,  0,-10],
+                        [-10, 10, 10, 10, 10, 10, 10,-10],
+                        [-10,  5,  0,  0,  0,  0,  5,-10],
+                        [-20,-10,-10,-10,-10,-10,-10,-20]
+                    ],
+                'R': [
+                        [0,  0,  0,  0,  0,  0,  0,  0],
+                        [5, 10, 10, 10, 10, 10, 10,  5],
+                        [-5,  0,  0,  0,  0,  0,  0, -5],
+                        [-5,  0,  0,  0,  0,  0,  0, -5],
+                        [-5,  0,  0,  0,  0,  0,  0, -5],
+                        [-5,  0,  0,  0,  0,  0,  0, -5],
+                        [-5,  0,  0,  0,  0,  0,  0, -5],
+                        [0,  0,  0,  5,  5,  0,  0,  0]
+                    ],
+                'Q':[
+                        [-20,-10,-10, -5, -5,-10,-10,-20],
+                        [-10,  0,  0,  0,  0,  0,  0,-10],
+                        [-10,  0,  5,  5,  5,  5,  0,-10],
+                        [-5,  0,  5,  5,  5,  5,  0, -5],
+                        [0,  0,  5,  5,  5,  5,  0, -5],
+                        [-10,  5,  5,  5,  5,  5,  0,-10],
+                        [-10,  0,  5,  0,  0,  0,  0,-10],
+                        [-20,-10,-10, -5, -5,-10,-10,-20]
+                    ],
+                'K':[
+                        [-30,-40,-40,-50,-50,-40,-40,-30],
+                        [-30,-40,-40,-50,-50,-40,-40,-30],
+                        [-30,-40,-40,-50,-50,-40,-40,-30],
+                        [-30,-40,-40,-50,-50,-40,-40,-30],
+                        [-20,-30,-30,-40,-40,-30,-30,-20],
+                        [-10,-20,-20,-20,-20,-20,-20,-10],
+                        [20, 20,  0,  0,  0,  0, 20, 20],
+                        [20, 30, 10,  0,  0, 10, 30, 20]
+                    ]
+                }
 
 
 def choose_random_move(board: Board) -> Move:
@@ -27,12 +89,48 @@ def choose_random_move(board: Board) -> Move:
     return random.choice(legal) if legal else None
 
 
+def get_loc_score(board: Board, r: int, c: int) -> int:
+    """
+    add location bonus to heuristic function based on piece location on board.
+    :param board: current state of the board.
+    :param r: row index.
+    :param c: column index.
+    :return: location bonus score
+    """
+    if board.piece_at(r, c):
+        piece = board.piece_at(r, c)
+        piece_sign = 1 if piece[0] == 'w' else -1
+        return (PIECES_LOCATION_SCORE[piece[1]][r][c] + PIECES_SCORE[piece[1]]) * piece_sign
+    return 0
+
+
 def evaluate(board: Board) -> float:
-    """Return a heuristic score from White's perspective."""
-    raise NotImplementedError("Implement your evaluation function in ai.py")
+    """
+    Calculate heuristic score from White's perspective.
+    :param board: current state of the board.
+    :return: heuristic score
+    """
+
+    # check terminal state of game
+    if is_terminal(board):
+        if board.turn == 'white':
+            return float('inf')
+        return -float('inf')
+
+    score = 0
+    #gives pieces score based on their location
+    for r in range(8):
+        for c in range(8):
+            if board.piece_at(r, c):
+                score += get_loc_score(board, r, c)
+    return score
+
 
 
 def is_terminal(board: Board) -> bool:
+    """
+    Return True if the board is terminal (game over).
+    """
     res = board.outcome()
     if res is not None and res[0] == 'checkmate':
         return True
@@ -40,6 +138,13 @@ def is_terminal(board: Board) -> bool:
 
 
 def minmax_max_component(board: Board, depth: int, nodes_visited: Set[Board]) -> Tuple[float, Optional[Move]]:
+    """
+    Goes over all possibilities to move and return the one that that will give us the biggest value according to minmax algorithm
+    :param board: current state of the board.
+    :param depth: depth to go into game branches
+    :param nodes_visited: set of boards visited while calculating next move.
+    :return: tuple of best move score (highest) and move itself
+    """
     nodes_visited.add(board)
     if depth == 0 or is_terminal(board):
         return evaluate(board), None
@@ -48,7 +153,7 @@ def minmax_max_component(board: Board, depth: int, nodes_visited: Set[Board]) ->
     for move in board.legal_moves():
         new_board = board.clone()
         new_board.make(move)
-        value, made_move = minmax_min_component(new_board, move, depth - 1, nodes_visited)
+        value, made_move = minmax_min_component(new_board, depth - 1, nodes_visited)
         if value > best_score:
             best_score = value
             best_move = move
@@ -57,6 +162,13 @@ def minmax_max_component(board: Board, depth: int, nodes_visited: Set[Board]) ->
 
 
 def minmax_min_component(board: Board, depth: int, nodes_visited: Set[Board]) -> Tuple[float,  Optional[Move]]:
+    """
+    Goes over all possibilities to move and return the one that that will give us the smallest value according to minmax algorithm
+    :param board: current state of the board.
+    :param depth: depth to go into game branches
+    :param nodes_visited: set of boards visited while calculating next move.
+    :return: tuple of best move score (lowest) and move itself
+    """
     nodes_visited.add(board)
     if depth == 0 or is_terminal(board):
         return evaluate(board), None
@@ -65,7 +177,7 @@ def minmax_min_component(board: Board, depth: int, nodes_visited: Set[Board]) ->
     for move in board.legal_moves():
         new_board = board.clone()
         new_board.make(move)
-        value, made_move = minmax_min_component(new_board, move, depth - 1, nodes_visited)
+        value, made_move = minmax_min_component(new_board, depth - 1, nodes_visited)
         if value < best_score:
             best_score = value
             best_move = move
@@ -90,6 +202,16 @@ def choose_minimax_move(board: Board, depth: int=2, metrics=None) -> Tuple[Move,
 
 def alpha_beta_max_component(board: Board, depth: int, nodes_visited: Set[Board],
                              alpha: float = -float('inf'), beta: float = float('inf')) -> Tuple[float,  Optional[Move]]:
+    """
+    Goes over possibilities to move and return the one that that will give us the biggest value according to minmax algorithm.
+    Uses alpha-beta pruning to avoid exploring dead cause branches and save compute time
+    :param board: current state of the board.
+    :param depth: depth to go into game branches
+    :param nodes_visited: set of boards visited while calculating next move.
+    :param alpha: lower bound on the score we already secured in previous branches
+    :param beta: upper bound on the score we already secured in previous branches
+    :return: tuple of best move score (highest) and move itself
+    """
     nodes_visited.add(board)
     if depth == 0 or is_terminal(board):
         return evaluate(board), None
@@ -98,7 +220,7 @@ def alpha_beta_max_component(board: Board, depth: int, nodes_visited: Set[Board]
     for move in board.legal_moves():
         new_board = board.clone()
         new_board.make(move)
-        value, made_move = alpha_beta_min_component(new_board, move, depth - 1, nodes_visited, alpha, beta)
+        value, made_move = alpha_beta_min_component(new_board, depth - 1, nodes_visited, alpha, beta)
         if value > best_score:
             best_score = value
             best_move = move
@@ -111,6 +233,16 @@ def alpha_beta_max_component(board: Board, depth: int, nodes_visited: Set[Board]
 
 def alpha_beta_min_component(board: Board, depth: int, nodes_visited: Set[Board],
                              alpha: float = -float('inf'), beta: float = float('inf')) -> Tuple[float,  Optional[Move]]:
+    """
+    Goes over possibilities to move and return the one that that will give us the smallest value according to minmax algorithm.
+    Uses alpha-beta pruning to avoid exploring dead cause branches and save compute time.
+    :param board: current state of the board.
+    :param depth: depth to go into game branches
+    :param nodes_visited: set of boards visited while calculating next move.
+    :param alpha: lower bound on the score we already secured in previous branches
+    :param beta: upper bound on the score we already secured in previous branches
+    :return: tuple of best move score (lowest) and move itself
+    """
     nodes_visited.add(board)
     if depth == 0 or is_terminal(board):
         return evaluate(board), None
@@ -119,7 +251,7 @@ def alpha_beta_min_component(board: Board, depth: int, nodes_visited: Set[Board]
     for move in board.legal_moves():
         new_board = board.clone()
         new_board.make(move)
-        value, made_move = alpha_beta_min_component(new_board, move, depth - 1, nodes_visited, alpha, beta)
+        value, made_move = alpha_beta_min_component(new_board, depth - 1, nodes_visited, alpha, beta)
         if value < best_score:
             best_score = value
             best_move = move
